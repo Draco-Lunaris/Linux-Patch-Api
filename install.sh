54440f124ea38e98365128ce7f07f3b5c18bce8a
Draco-Lunaris-Echo <draco-lunaris-echo@moon-dragon.us>
2026-06-18 21:35:12 -0500

    chore: bump version to 1.5.0
    Stable release of the self-update feature. Includes:
    - Self-update via detached systemd unit downloading from GitHub Releases
    - Packaging permission fixes for RPM, Arch, Alpine
    - Pre-release version format handling for all package formats
    - Self-update files shipped in all package formats
    - OpenRC init script status fix

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ NC='\033[0m' # No Color
 
 # Configuration
 APP_NAME="linux-patch-api"
-VERSION="1.5.0-rc6"
+VERSION="1.5.0"
 INSTALL_DIR="/usr/bin"
 CONFIG_DIR="/etc/linux_patch_api"
 CERTS_DIR="${CONFIG_DIR}/certs"
