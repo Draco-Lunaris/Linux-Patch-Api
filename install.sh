e3815e279fe2cd0c30593f52f39f27f397924919
Draco-Lunaris-Echo <draco-lunaris-echo@moon-dragon.us>
2026-07-01 14:58:37 -0500

    chore: bump version to 2.0.1

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ NC='\033[0m' # No Color
 
 # Configuration
 APP_NAME="linux-patch-api"
-VERSION="1.5.8"
+VERSION="2.0.1"
 INSTALL_DIR="/usr/bin"
 CONFIG_DIR="/etc/linux_patch_api"
 CERTS_DIR="${CONFIG_DIR}/certs"
