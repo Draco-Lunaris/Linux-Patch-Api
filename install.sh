0593f3c8b62897b0166f96505b6e04c1b9107fec
Draco-Lunaris-Echo <Draco-Lunaris-Echo@moon-dragon.us>
2026-07-14 16:46:43 +0000

    release: v2.1.2

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ NC='\033[0m' # No Color
 
 # Configuration
 APP_NAME="linux-patch-api"
-VERSION="2.1.1"
+VERSION="2.1.2"
 INSTALL_DIR="/usr/bin"
 CONFIG_DIR="/etc/linux_patch_api"
 CERTS_DIR="${CONFIG_DIR}/certs"
