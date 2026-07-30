e5592a3b3bdce95617bdcd75506cb38b5fd2a499
Kelly Slavens <kwslavens@gmail.com>
2026-07-30 15:29:59 -0500

    chore: bump version to 2.6.7

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ NC='\033[0m' # No Color
 
 # Configuration
 APP_NAME="linux-patch-api"
-VERSION="2.4.0"
+VERSION="2.6.7"
 INSTALL_DIR="/usr/bin"
 CONFIG_DIR="/etc/linux_patch_api"
 CERTS_DIR="${CONFIG_DIR}/certs"
